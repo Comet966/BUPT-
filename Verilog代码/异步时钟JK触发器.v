de0b02aa9b9fb978b2da62df55c423fec62d9618
Comet966 <2803002402@qq.com>
2025-12-10 14:38:29 +0800

    Update 异步时钟JK触发器.v

--- Verilog代码/异步时钟JK触发器.v
+++ Verilog代码/异步时钟JK触发器.v
@@ -1,1 +1,18 @@
-﻿//异步时钟JK触发器module fpga_prj(j,k,rst,clk,q,q_bar);input j,k,rst,clk;output q,q_bar;reg q_value;assign q_bar = ~q_value;assign q = q_value;always@(negedge clk or posedge rst)beginif(rst == 1) begin q_value <= 0;endelse begin q_value <= (~j && q_value)||(k && ~q_value); endendendmodule
+﻿//异步时钟JK触发器
+module fpga_prj(j,k,rst,clk,q,q_bar);
+input j,k,rst,clk;
+output q,q_bar;
+
+reg q_value;
+assign q_bar = ~q_value;
+assign q = q_value;
+
+always@(negedge clk or posedge rst)begin
+
+
+if(rst == 1) begin q_value <= 0;end
+else begin q_value <= (~j && q_value)||(k && ~q_value); end
+
+end
+
+endmodule
