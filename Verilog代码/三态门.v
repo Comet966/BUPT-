3b7d528365711138665966a82857337d006834e4
Comet966 <2803002402@qq.com>
2025-12-10 12:59:51 +0800

    Update 三态门.v

--- Verilog代码/三态门.v
+++ Verilog代码/三态门.v
@@ -1,1 +1,11 @@
-﻿//使用三目运算符实现三态门module fpga_prj2(sw,key,led);input [3:0]sw;input [3:0] key;output [7:0] led;assign led[7:4] = 4'b1111;//使用sw[0]作为使能信号assign led[3:0] = sw[0] ? key[3:0] : 4'bzzzz;//等价于4'bzendmodule
+﻿//使用三目运算符实现三态门
+module fpga_prj2(sw,key,led);
+input [3:0]sw;
+input [3:0] key;
+output [7:0] led;
+
+assign led[7:4] = 4'b1111;
+//使用sw[0]作为使能信号
+assign led[3:0] = sw[0] ? key[3:0] : 4'bzzzz;//等价于4'bz
+
+endmodule
